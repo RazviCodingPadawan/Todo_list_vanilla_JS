2b989bc6b1a90ce5b12a52de4fb4b80813210f91
Razvan <razvan.georgescu89@yahoo.com>
2021-11-06 17:42:24 +0100

    tema continue

---
+++ js/main.js
@@ -0,0 +1,59 @@
+/* let main = document.getElementById('content'); */
+/* let btn = document.getElementById('add');
+
+btn.addEventListener('click', function() {
+    let newSection = document.createElement('section');
+    newSection.id = 'first-section';
+    console.log(newSection);
+    document.getElementById('content').append(newSection);
+}) */
+
+/* let main = document.getElementById('content');
+
+
+let btn = document.getElementById('add');
+btn.addEventListener('click', function() {
+    let newList = document.createElement('section');
+    newList.innerHTML = '<h2 id="toDO">Chores to be done (pending)</h2>';
+    newList.innerHTML += '<div class="list</div>">';
+    console.log(newList);
+    main.appendChild(newList);
+}) */
+
+/* var input = document.getElementById('add-new-item');
+let btn = document.getElementById('add');
+btn.addEventListener('click', function(e){
+    e.target.parentNode.sElementChild.remove();
+}) */
+
+let firstList = document.getElementById('firstList');
+let addBtn = document.getElementById('add');
+
+
+addBtn.addEventListener('click', function() {
+    let newItem = document.createElement('li');
+    firstList.appendChild(newItem);
+
+    let newInput = document.getElementById('add-new-item');
+
+    let input = document.createElement('input');
+    newItem.appendChild(input);
+    input.placeholder = newInput.value;
+    input.disabled = true;
+
+    let changeBtn = document.createElement('button');
+    newItem.appendChild(changeBtn);
+    changeBtn.innerHTML = "Change";
+
+    let doneBtn = document.createElement('button');
+    newItem.appendChild(doneBtn);
+    doneBtn.innerHTML = "Done";
+
+    let deleteBtn = document.createElement('button');
+    newItem.appendChild(deleteBtn);
+    deleteBtn.innerHTML = "Delete";
+
+   //firstList.innerHTML += '<li><input type="text" disabled> <button id="change">Change</button><button id="done">Done</button><button id="delete">Delete</button></li>';
+});
+
+
